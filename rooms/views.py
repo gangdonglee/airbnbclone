7c4a6cff7feee7654658310cbf258b66f9faa803
gangdonglee <leegangdong@naver.com>
2021-02-15 21:46:21 +0900

    0215

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,17 @@
+from math import ceil
 from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
+from . import models
 
 
 def all_rooms(request):
-    now = datetime.now()
-    return HttpResponse(content=f"{now}")
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.get_page(int(page))
+    return render(
+        request,
+        "rooms/home.html",
+        context={"page": rooms},
+    )
